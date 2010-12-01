f8b6c913457c24c7502bbf8d0133ceca3f22dedd
Michael Barton <mail@michaelbarton.me.uk>
2010-12-01 18:47:37 -0500

    Output type method for scaffolder dsl

--- lib/genomer/rules_dsl.rb
+++ lib/genomer/rules_dsl.rb
@@ -1,5 +1,9 @@
 class Genomer::RulesDSL
 
+  def initialize
+    @types = Array.new
+  end
+
   def scaffold_file(location=nil)
     return @scaffold if location.nil?
     @scaffold = location
@@ -8,6 +12,11 @@ class Genomer::RulesDSL
   def sequence_file(location=nil)
     return @sequence if location.nil?
     @sequence = location
+  end
+
+  def output(*types)
+    return @types if types.empty?
+    @types.concat types
   end
 
 end
--- spec/genomer/rules_dsl_spec.rb
+++ spec/genomer/rules_dsl_spec.rb
@@ -6,16 +6,36 @@ describe Genomer::RulesDSL do
     @dsl = Genomer::RulesDSL.new
   end
 
-  describe "methods" do
+  describe "file location attributes" do
 
-    it "should include scaffold_file attribute" do
+    it "should include scaffold_file" do
       @dsl.scaffold_file "location"
       @dsl.scaffold_file.should == "location"
     end
 
-    it "should include sequence_file attribute" do
+    it "should include sequence_file" do
       @dsl.sequence_file "location"
       @dsl.sequence_file.should == "location"
+    end
+
+  end
+
+  describe "output types method" do
+
+    it "should allow single output types to be set" do
+      @dsl.output :type
+      @dsl.output.should == [:type]
+    end
+
+    it "should allow multipe output types to be set" do
+      @dsl.output :type1, :type2
+      @dsl.output.should == [:type1, :type2]
+    end
+
+    it "should allow incremental addition of types" do
+      @dsl.output :type1
+      @dsl.output :type2
+      @dsl.output.should == [:type1, :type2]
     end
 
   end

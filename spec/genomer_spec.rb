8ae87202421b9db3246ec7a97f7c680de0900cf4
Michael Barton <mail@michaelbarton.me.uk>
2010-12-01 23:35:57 -0500

    Genomer binary calls runtime class

--- spec/genomer_spec.rb
+++ spec/genomer_spec.rb
@@ -2,6 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "the genomer binary" do
 
+  before(:each) do
+    @empty_rules = Tempfile.new('genomer').path
+  end
+
   it "should exist" do
     File.exists?(GENOMER).should be_true
   end
@@ -10,8 +14,8 @@ describe "the genomer binary" do
     File.executable?(GENOMER).should be_true
   end
 
-  it "should return a 0 exit when run without arguments" do
-    clean = system GENOMER
+  it "should return a 0 exit when run with an empty Rules file" do
+    clean = system(GENOMER,Tempfile.new('genomer').path)
     clean.should be_true
     $?.should == 0
   end

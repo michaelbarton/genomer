e1a8e3413e83e68da43a5fa17c08f3cdeafc3a4d
Michael Barton <mail@michaelbarton.me.uk>
2010-12-02 15:24:23 -0500

    Fix support requires in spec/helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'tempfile'
 require 'rspec'
 require 'fakefs/spec_helpers'
@@ -9,7 +10,9 @@ require 'genomer'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each do |f|
+  require File.expand_path(f)
+end
 
 RSpec.configure do |config|
   config.mock_with :mocha

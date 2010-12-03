2699125876fc4c690f5008227241b061040d2a96
Michael Barton <mail@michaelbarton.me.uk>
2010-12-02 21:32:56 -0500

    Refactor sequence content feature step

--- features/step_definitions/genomer_steps.rb
+++ features/step_definitions/genomer_steps.rb
@@ -29,7 +29,5 @@ Then /^the file "([^"]*)" should exist$/ do |file|
 end
 
 Then /^"([^"]*)" should contain the sequence "([^"]*)"$/ do |file,seq|
-  Bio::FlatFile.auto(file) do |ff|
-    ff.first.seq.upcase.should == seq.upcase
-  end
+  seq.upcase.should == output_sequence(File.read(file)).upcase
 end

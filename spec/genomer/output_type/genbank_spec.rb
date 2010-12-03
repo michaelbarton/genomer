27fc8337e8774a8e6ef9d4d900430f30902ea9c8
Michael Barton <mail@michaelbarton.me.uk>
2010-12-02 20:54:38 -0500

    Implement genbank output type

---
+++ spec/genomer/output_type/genbank_spec.rb
@@ -0,0 +1,14 @@
+require File.join(File.dirname(__FILE__),'..','..','spec_helper')
+
+describe Genomer::OutputType::Genbank do
+
+  subject do
+    rules = simple_rules [{'name' => 'seq1', 'nucleotides' => 'ATGC' }]
+    described_class.new(rules)
+  end
+
+  it{ should generate_the_sequence('ATGC') }
+  it{ described_class.should define_the_suffix_constant_as('gb') }
+  it{ described_class.should subclass_genomer_output_type }
+
+end

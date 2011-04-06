081a32a073f4648959232599d8f985a306037a12
Michael Barton <mail@michaelbarton.me.uk>
2011-04-05 21:38:21 -0400

    Refactor DSL to use classy-adornments

--- lib/genomer/rules_dsl.rb
+++ lib/genomer/rules_dsl.rb
@@ -1,35 +1,16 @@
+require 'classy_adornments'
+
 class Genomer::RulesDSL
+  include ClassyAdornments
 
-  def initialize
-    @types = Array.new
-  end
+  adorn :scaffold_file
+  adorn :sequence_file
+  adorn :annotation_file
 
-  private
+  adorn :out_file_name
+  adorn :out_dir_name
 
-  def self.attribute(name)
-    define_method(name) do |*arg|
-      var = "@#{name}"
-      unless arg.first # Is an argument is passed to the method?
-        value = instance_variable_get(var)
-        return value if value
-      end
-      instance_variable_set(var,arg.first)
-    end
-  end
-
-  attribute :scaffold_file
-  attribute :sequence_file
-  attribute :annotation_file
-
-  attribute :out_file_name
-  attribute :out_dir_name
-
-  public
-
-  def output(*types)
-    return @types if types.empty?
-    @types.concat types
-  end
+  adorn :output, :array => true
 
   def identifier(arg=nil)
     return @identifier if arg.nil?

4fcb007c10d04330c1f0fcef78776c651733c5e2
Michael Barton <mail@michaelbarton.me.uk>
2011-06-06 18:42:16 -0400

    Refactor method for setting metadata

--- lib/genomer/output_type/genbank.rb
+++ lib/genomer/output_type/genbank.rb
@@ -2,10 +2,17 @@ class Genomer::OutputType::Genbank < Genomer::OutputType
 
   SUFFIX = 'gb'
 
+  METADATA_MAP = {
+    :entry_id => :identifier,
+    :definition => :description
+  }
+
   def generate
     build = Bio::Sequence.new(sequence)
-    build.entry_id = @rules.identifier if @rules.identifier
-    build.definition = @rules.description if @rules.description
+    METADATA_MAP.each do |a,b|
+      value = @rules.send(b)
+      build.send(a.to_s + '=',value) unless value.nil?
+    end
     build.output(:genbank)
   end
 

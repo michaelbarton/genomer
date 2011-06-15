c00cf3c6d5005d60c7826efa18e823467c406038
Michael Barton <mail@michaelbarton.me.uk>
2011-06-15 15:15:07 -0400

    Can set identifier in fasta files

--- lib/genomer/output_type/fasta.rb
+++ lib/genomer/output_type/fasta.rb
@@ -3,7 +3,9 @@ class Genomer::OutputType::Fasta < Genomer::OutputType
   SUFFIX = 'fna'
 
   def generate
-    Bio::Sequence.new(sequence).output(:fasta)
+    options = Hash.new
+    options[:header] = @rules.identifier if @rules.identifier
+    Bio::Sequence.new(sequence).output(:fasta,options)
   end
 
 end

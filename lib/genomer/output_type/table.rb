0b61a785261f34e9ed3c21aba98616bbe10015ff
Michael Barton <mail@michaelbarton.me.uk>
2011-06-21 14:56:08 -0400

    Table no longer uses annotation instance variable

--- lib/genomer/output_type/table.rb
+++ lib/genomer/output_type/table.rb
@@ -17,7 +17,7 @@ class Genomer::OutputType::Table < Genomer::OutputType
     indent    = delimiter * 2
 
     out = [%W|>Feature #{identifier} annotation_table|]
-    @annotations.each do |attn|
+    annotations.each do |attn|
       out << [attn.start,attn.end,attn.feature]
       attn.attributes.each do |attr|
         if attr.first == 'ID'
@@ -34,7 +34,7 @@ class Genomer::OutputType::Table < Genomer::OutputType
 
   def prefix_annotation_id_field
     if @rules.annotation_id_field_prefix
-      @annotations.each do |attn|
+      annotations.each do |attn|
         attn.id.insert(0,@rules.annotation_id_field_prefix)
       end
     end
@@ -42,14 +42,14 @@ class Genomer::OutputType::Table < Genomer::OutputType
 
   def reset_annotation_id_field
     if @rules.reset_annotation_id_field?
-      @annotations.each_with_index do |annotation,count|
+      annotations.each_with_index do |annotation,count|
         annotation.id = sprintf("%06d",count+1)
       end
     end
   end
 
   def complement_reverse_strand_annotations
-    @annotations.each{|i| i.reverse if i.negative_strand? }
+    annotations.each{|i| i.reverse if i.negative_strand? }
   end
 
 end

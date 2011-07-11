cd42b651d33f92a8af6f0da6263b13b9edec4c74
Michael Barton <mail@michaelbarton.me.uk>
2011-07-11 11:57:59 -0400

    Genomer::Output::GFF3 class

---
+++ lib/genomer/output_type/gff3.rb
@@ -0,0 +1,5 @@
+class Genomer::OutputType::GFF3 < Genomer::OutputType
+
+  SUFFIX = 'gff'
+
+end
---
+++ spec/genomer/output_type/gff3_spec.rb
@@ -0,0 +1,10 @@
+require 'spec/spec_helper'
+
+describe Genomer::OutputType::GFF3 do
+
+  describe "class contants" do
+    it{ described_class.should define_the_suffix_constant_as('gff') }
+    it{ described_class.should subclass_genomer_output_type }
+  end
+
+end

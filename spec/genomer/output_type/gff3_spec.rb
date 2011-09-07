1aab772bb53c1d98870daf64dd169e1b136419df
Michael Barton <mail@michaelbarton.me.uk>
2011-09-07 16:37:44 -0400

    Fix spec helper require path

--- spec/genomer/output_type/gff3_spec.rb
+++ spec/genomer/output_type/gff3_spec.rb
@@ -1,4 +1,4 @@
-require 'spec/spec_helper'
+require 'spec_helper'
 
 describe Genomer::OutputType::Gff3 do
 
--- spec/genomer/runtime_spec.rb
+++ spec/genomer/runtime_spec.rb
@@ -1,4 +1,4 @@
-require 'spec/spec_helper'
+require 'spec_helper'
 
 describe Genomer::Runtime do
   include FakeFS::SpecHelpers

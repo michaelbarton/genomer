678294b0ce89da4fe46634434725d45c51442e62
Michael Barton <mail@michaelbarton.me.uk>
2011-09-13 22:32:34 -0400

    Feature initing project with existing directory.

--- features/step_definitions/genomer_steps.rb
+++ features/step_definitions/genomer_steps.rb
@@ -1,4 +1,4 @@
 Given /^I run the genomer command with the arguments "([^"]*)"$/ do |args|
   genomer = File.join %W| #{File.dirname(__FILE__)} .. .. bin genomer|
-  Given "I successfully run `#{genomer} #{args}`"
+  Given "I run `#{genomer} #{args}`"
 end

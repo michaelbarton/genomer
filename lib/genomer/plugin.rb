ce5710eee211db275102e569fedfea3a69d245ce
Michael Barton <mail@michaelbarton.me.uk>
2011-12-02 16:23:15 -0500

    Move #to_class_name to plugin

--- lib/genomer/plugin.rb
+++ lib/genomer/plugin.rb
@@ -1,4 +1,7 @@
-class Genomer
-  class Plugin
+class Genomer::Plugin
+
+  def self.to_class_name(string)
+    string.split('-').map{|i| i.capitalize}.join
   end
+
 end
--- spec/genomer/plugin_spec.rb
+++ spec/genomer/plugin_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Genomer::Plugin do
+
+  describe "#to_class_name" do
+
+    it "should dash separated words to camel case" do
+      described_class.to_class_name('words-with-dashes').should == "WordsWithDashes"
+    end
+
+  end
+
 end

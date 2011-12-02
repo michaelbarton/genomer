c79a2375b68fae165d5fdb9d1cc78500fef51340
Michael Barton <mail@michaelbarton.me.uk>
2011-12-02 16:46:28 -0500

    Move #plugins method to plugin

--- lib/genomer/plugin.rb
+++ lib/genomer/plugin.rb
@@ -1,5 +1,10 @@
 class Genomer::Plugin
 
+  def self.plugins
+    require 'bundler'
+    Bundler.setup.gems.select{|gem| gem.name =~ /genomer-plugin-.+/ }
+  end
+
   def self.to_class_name(string)
     string.split('-').map{|i| i.capitalize}.join
   end
--- spec/genomer/plugin_spec.rb
+++ spec/genomer/plugin_spec.rb
@@ -2,6 +2,55 @@ require 'spec_helper'
 
 describe Genomer::Plugin do
 
+  describe "#plugins" do
+
+    before do
+      mock(Bundler).setup do
+        mock!.gems{ gems }
+      end
+    end
+
+    describe "where a single genomer plugin is specified" do
+
+      let(:gems) do
+        [Gem::Specification.new do |s|
+          s.name = 'genomer-plugin-simple'
+        end]
+      end
+
+      it "should return the genomer plugin" do
+        described_class.plugins.should == gems
+      end
+
+    end
+
+    describe "where no gems at all are specified" do
+
+      let(:gems) do
+        []
+      end
+
+      it "should return an empty array" do
+        described_class.plugins.should be_empty
+      end
+
+    end
+
+    describe "where one plugin and one non-plugin are specified" do
+
+      let(:gems) do
+        [Gem::Specification.new{|s| s.name = 'genomer-plugin-simple' },
+         Gem::Specification.new{|s| s.name = 'not-a-plugin' }]
+      end
+
+      it "should return the genomer plugin" do
+        described_class.plugins.should == [gems.first]
+      end
+
+    end
+
+  end
+
   describe "#to_class_name" do
 
     it "should dash separated words to camel case" do

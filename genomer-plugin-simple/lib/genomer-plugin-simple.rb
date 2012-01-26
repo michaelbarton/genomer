35b4c849ca053df73954a637a74c59b9d7805206
Michael Barton <mail@michaelbarton.me.uk>
2012-01-26 07:51:23 -0500

    Refactor echo arg for clarity in simple-plugin

--- genomer-plugin-simple/lib/genomer-plugin-simple.rb
+++ genomer-plugin-simple/lib/genomer-plugin-simple.rb
@@ -3,7 +3,7 @@ class GenomerPluginSimple < Genomer::Plugin
   def run
     case arguments.shift
     when nil           then 'Plugin "simple" called'
-    when 'echo'        then @arguments.unshift('Echo:').join(' ')
+    when 'echo'        then "Echo: #{arguments.join(' ')}"
     when 'describe'    then "The scaffold contains #{scaffold.length} entries"
     when 'annotations' then annotations.inject("##gff-version 3\n"){|s, a| s << a.to_s}
     end
